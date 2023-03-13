f873a60e482e202bd8a64a61aba7fae615835acc
Vitaly Z <v.zalivnov@puls.ru>
2023-03-14 01:43:21 +0300

    add models

--- src/models/film.py
+++ src/models/film.py
@@ -1,19 +1,35 @@
+from typing import Optional
+
 import orjson
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+
 from pydantic import BaseModel
 
 
 def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
 
-class Film(BaseModel):
+class Person(BaseModel):
     id: str
-    title: str
-    description: str
+    name: str
 
     class Config:
-        # Заменяем стандартную работу с json на более быструю
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Film(BaseModel):
+    actors: Optional[list[Person]]
+    actors_name: Optional[str]
+    id: str
+    title: Optional[str]
+    description: Optional[str] = None
+    director: Optional[str]
+    genre: Optional[str]
+    imdb_rating: Optional[float]
+    writers: Optional[list[Person]]
+    writers_names: Optional[str]
+
+    class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
---
+++ src/models/genre.py
@@ -0,0 +1,18 @@
+import orjson
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+
+class Genre(BaseModel):
+    id: str
+    name: str
+    description: Optional[str] = None
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
